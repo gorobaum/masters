f96330372e6995e1dce007a8efd1aceae4acff94
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2014-01-13 13:01:58 -0200

    Starting the developmento of the demons algorithm

--- code/imageDeformation.py
+++ code/imageDeformation.py
@@ -1,12 +1,20 @@
 from PIL import Image
+import sys
 import transformation as Transformations
 
+print sys.argv[1]
+
 try:
-	original = Image.open("../testImages/msc.jpg")
+	original = Image.open(sys.argv[1])
 except:
 	print "Unabel to load image!"
 
-print "The size of the image is:"
+print "The size of the original image is:"
 print(original.format, original.size, original.mode)
-out = Transformations.affine(original, -0.1, 0.1)
-out.show()
+
+modified = Transformations.affine(original, -0.5, 0)
+
+print "The size of the modified image is:"
+print(modified.format, modified.size, modified.mode)
+
+modified.show()
