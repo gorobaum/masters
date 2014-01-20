8fcf673eb01f9ec4e34fb1fdb080d25c12a6dc52
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2014-01-20 15:17:38 -0200

    New function

--- code/demons.py
+++ code/demons.py
@@ -13,13 +13,10 @@ def demons(movingImage, staticImage):
 	
 	width, height = deformedImage.size
 	displVectors = createDisplVectors(width, height)
-	cont = True
 	iteration = 0
 	start_time = time.time()
-	# while cont:
 	for a in range(100):
 		loop_time = time.time()
-		cont = False
 		print 'Iteration number ' + str(iteration)
 		for x in range(width):
 			for y in range(height):
@@ -28,35 +25,29 @@ def demons(movingImage, staticImage):
 				mix = x-displVectors[i][0]
 				miy = y-displVectors[i][1]
 				deformedPixels[x, y] = aux.bilinearInterpolation(movingPixels, mix, miy, width, height)
-				# update displacement vector x
-				divx = (pow(gradients[i][0], 2) + pow(gradients[i][1], 2) + pow((deformedPixels[x, y] - staticPixels[x, y]), 2))
-				if divx == 0:
-					displVectors[i][0] = 0
-				else: 
-					newDisplX = displVectors[i][0] + (deformedPixels[x, y] - staticPixels[x, y])*gradients[i][0]/divx
-					if not cont and (round(displVectors[i][0] - newDisplX) != 0):
-						cont = True
-						displVectors[i][0] = displVectors[i][0] + newDisplX
-				# update displacement vector y
-				divy = (pow(gradients[i][0], 2) + pow(gradients[i][1], 2) + pow((deformedPixels[x, y] - staticPixels[x, y]), 2))
-				if divy == 0:
-					displVectors[i][1] = 0
-				else:
-					newDisplY = displVectors[i][1] + (deformedPixels[x, y] - staticPixels[x, y])*gradients[i][1]/divy
-					if not cont and (round(displVectors[i][1] - newDisplY) != 0):
-						cont = True
-						displVectors[i][1] = displVectors[i][1] + newDisplY
+				# update displvector
+				updateDisplVector(displVectors, gradients, deformedPixels, staticPixels, i, x ,y)
 		displVectors = ndimage.filters.gaussian_filter(displVectors, 1)
 		print "iteration ", iteration, "took ", time.time() - loop_time, "seconds."
-		iteration += 1
 		imageName = "result" + str(iteration) + ".jpg"
+		iteration += 1
 		deformedImage.save(imageName)
+
+def updateDisplVector(displVectors, gradients, deformedPixels, staticPixels, i, x ,y):
+	print deformedPixels[x, y], staticPixels[x, y]
+	div = (pow(gradients[i][0], 2) + pow(gradients[i][1], 2) + pow((deformedPixels[x, y] - staticPixels[x, y]), 2))
+	newDisplX = displVectors[i][0] + (deformedPixels[x, y] - staticPixels[x, y])*gradients[i][0]/div
+	if displVectors[i][0] - newDisplX > 1e-10:
+		displVectors[i][0] = displVectors[i][0] + newDisplX
+	newDisplY = displVectors[i][1] + (deformedPixels[x, y] - staticPixels[x, y])*gradients[i][1]/div
+	if displVectors[i][1] - newDisplY > 1e-10:
+		displVectors[i][1] = displVectors[i][1] + newDisplY
 
 def findGrad(image):
 	dx = ImageFilter.Kernel((3, 3), (-1,0,1,-1,0,1,-1,0,1), scale=None, offset=0)
 	dy = ImageFilter.Kernel((3, 3), (-1,-1,-1,0,0,0,1,1,1), scale=None, offset=0)
 	xImage = image.filter(dx)
-	yImage = image.filter(dy)
+	yImage = xImage.filter(dy)
 	gradX = list(xImage.getdata())
 	gradY = list(yImage.getdata())
 	grad = zip(gradX, gradY)
