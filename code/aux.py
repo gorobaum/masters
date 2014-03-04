0eeeafd7eeacad283d8e1ef872385e1dc309bd4b
Thiago Nunes <gorobaum@gmail.com>
2014-03-04 13:34:36 -0300

    Now I can choose which type of interpolation

--- code/aux.py
+++ code/aux.py
@@ -11,14 +11,17 @@ def sumOfField(ndarray):
 			result = result + abs(ndarray[y][x][0]) + abs(ndarray[y][x][1])
 	return result
 
-def deformSin(imagePixels):
+def deformSin(imagePixels, typeOfInter):
 	deformedPixels = numpy.ndarray(imagePixels.shape, dtype=int32)
 	h, w = imagePixels.shape
 	for i in range(h):
 		for j in range(w):
-			newY = i + math.sin(j/16)
+			newY = i + 2*math.sin(j/16)
 			newX = j 
-			deformedPixels[i,j] = interpolation.bilinear(imagePixels, newX, newY, w, h)
+			if typeOfInter == "-b":
+				deformedPixels[i,j] = interpolation.bilinear(imagePixels, newX, newY, w, h)
+			elif typeOfInter == "-n":
+				deformedPixels[i,j] = interpolation.nearestNeighbor(imagePixels, newX, newY, w, h)
 	return deformedPixels
 
 def saveVectorField(width, height, xVec, yVec, imageName):
@@ -30,3 +33,14 @@ def saveVectorField(width, height, xVec, yVec, imageName):
 
 	title(imageName)
 	savefig(imageName+".jpg")
+
+def saveHistogram(imagePixels, histName, fileName):
+	# the histogram of the data
+	plt.hist(imagePixels.flatten(), 255, facecolor='green', alpha=0.75)
+
+	plt.xlabel('Pixels')
+	plt.ylabel('Appearence')
+	plt.title(histName)
+	plt.grid(True)
+
+	plt.savefig(fileName)
--- code/imageDeformation.py
+++ code/imageDeformation.py
@@ -4,14 +4,17 @@ import scipy
 from scipy import ndimage
 
 if len(sys.argv) <= 1:
-	print "Please pass the image name as an arg"
+	print "Please pass the image name as an arg and the type of interpolation"
 	sys.exit()
 else:
 	try:
 		original = scipy.misc.imread(sys.argv[1], True)
+		typeOfInterpolation = sys.argv[2]
 	except:
 		print "Unable to load image!"
 
-	modified = aux.deformSin(original)
+	aux.saveHistogram(original, "Original Image Histogram", "original-teste-h.jpg")
+	modified = aux.deformSin(original, typeOfInterpolation)
 	scipy.misc.imsave("../testImages/modified-teste.jpg", modified)
+	aux.saveHistogram(modified, "Modified Image Histogram", "modified-teste-h"+typeOfInterpolation+".jpg")
 	print "Transformation sucessful with the name modified-teste.jpg"
